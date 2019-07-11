00a23943683fc5bac6f1d9a5cbf4f76c8452b50a
dickovickjustin <justin.dickovick@student.fairfield.edu>
2019-07-11 10:50:14 -0400

    latest

--- DcoDe/run.C
+++ DcoDe/run.C
@@ -1,7 +1,7 @@
 {
-//TFile *f=new TFile("40.root");
-TChain *c=new TChain("hiporoot");
-c->Add("40.root");
-c->Process("DCoDe.C");
+TFile *f=new TFile("/home/justind/DATA/40.root");
+TChain *c=new TChain("hipotree");
+c->Add("/home/justind/DATA/40.root");
+c->Process("DcoDe.C");
 
 }
